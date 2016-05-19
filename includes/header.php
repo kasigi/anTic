a70c1a7ef12d4d1ec23884c6eaece15a24e98a3d
Tor N. Johnson <kasigi@gmail.com>
2016-05-19 14:58:55 -0400

    more patches

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="shortcut icon" href="favicon.png" />
+    <!--<link rel="shortcut icon" href="favicon.png" />-->
     <link rel="icon" href="favicon.png" />
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel='stylesheet' href='http://www.kasigi.com/wp-content/themes/tnjPortfolio2013/fonts/junicode.css?ver=4.4.2' type='text/css' media='all' />
