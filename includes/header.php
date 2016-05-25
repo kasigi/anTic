5e6d14b61faa0895f591f5294abe74e1b5b00645
Tor N. Johnson <kasigi@gmail.com>
2016-05-25 16:33:57 -0400

    Visual updates to buttons and font-sizes for buttons

--- includes/header.php
+++ includes/header.php
@@ -6,8 +6,8 @@
     <link rel="icon" href="favicon.png" />
     <!--<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">-->
     <link rel='stylesheet' href='css/fonts/junicode.css' type='text/css' media='all' />
-    <link rel='stylesheet' id='fontUbuntu-css'  href='//fonts.googleapis.com/css?family=Ubuntu%3A300%2C400%2C500%2C700%2C400italic%2C500italic&#038;' type='text/css' media='all' />
-
+    <link rel='stylesheet' href='//fonts.googleapis.com/css?family=Ubuntu%3A300%2C400%2C500%2C700%2C400italic%2C500italic&#038;' type='text/css' media='all' />
+    <link rel='stylesheet' href='//fonts.googleapis.com/css?family=Source+Sans+Pro%3A300%2C400%2C600%2C700%2C900%2C400italic%2C600italic&#038;' type='text/css' media='all' />
     <link href="css/styles.css" rel="stylesheet">
 
 
